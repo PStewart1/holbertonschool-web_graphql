272a5ea02c54aa24a40304d4538baf4e11131d49
PStewart1 <4920@holbertonstudents.com>
2023-11-03 11:05:16 -0500

    Root Query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,9 +1,6 @@
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
-
-const TaskType = new GraphQLObjectType;
-
-({
+const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: {
     id: { type: GraphQLString },
@@ -11,6 +8,22 @@ const TaskType = new GraphQLObjectType;
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
   },
-}) = TaskType;
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+      },
+    },
+  },
+});
 
 module.exports = TaskType;
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
